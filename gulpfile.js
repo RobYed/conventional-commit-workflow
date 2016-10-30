719ea1e150f9e1b1d67d76445d7ad15e1c781019
Robert Dey <robdey88@gmail.com>
2016-10-30 08:12:11 +0100

    chore(build): add gulp task for changelog creation

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp                  = require('gulp');
+const conventionalChangelog = require('gulp-conventional-changelog');
+
+
+gulp.task('changelog', function () {
+  return gulp.src('CHANGELOG.md')
+    .pipe(conventionalChangelog({
+      // conventional-changelog options go here
+      preset: 'angular'
+    }, {
+      // context goes here
+    }, {
+      // git-raw-commits options go here
+    }, {
+      // conventional-commits-parser options go here
+    }, {
+      // conventional-changelog-writer options go here
+    }))
+    .pipe(gulp.dest('./'));
+});
