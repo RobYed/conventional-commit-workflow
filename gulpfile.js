fa78abbe1057d3eb9355d7dfb0bdc115cb663d71
Robert Dey <robdey88@gmail.com>
2016-10-30 08:27:09 +0100

    chore(build): add conventional version bump

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-const gulp                  = require('gulp');
-const conventionalChangelog = require('gulp-conventional-changelog');
+const gulp                        = require('gulp');
+const conventionalChangelog       = require('gulp-conventional-changelog');
+const conventionalRecommendedBump = require('conventional-recommended-bump');
+const bump                        = require('gulp-bump');
 
 
 gulp.task('changelog', function () {
@@ -17,4 +19,23 @@ gulp.task('changelog', function () {
       // conventional-changelog-writer options go here
     }))
     .pipe(gulp.dest('./'));
+});
+
+gulp.task('version-bump', function(done) {
+  /**
+   * Bumping version number and tagging the repository with it.
+   * Please read http://semver.org/
+   *
+   * To bump the version numbers accordingly after you did a patch,
+   * introduced a feature or made a backwards-incompatible release.
+   */
+
+  conventionalRecommendedBump({ preset: 'angular' }, function (err, importance) {
+    // Get all the files to bump version in
+    gulp.src('./package.json')
+      .pipe(bump({ type: importance }))
+      .pipe(gulp.dest('./'));
+
+    done();
+  });
 });
